c7c9732500d012b8716a64f0857e67b5610c20e9
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-06-19 20:50:55 +0300

    deleting unused files

--- src/components/redux/phonebook/contsacts-actions.jsx
+++
@@ -1,9 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-const filterChange = createAction('phonebook/filterChange');
-
-const contactsActions = {
-  filterChange,
-};
-
-export default contactsActions;
