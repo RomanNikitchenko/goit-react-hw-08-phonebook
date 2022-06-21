c7dce944707e1cec400e4a415e44f21fb9fad1e6
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-06-21 16:53:42 +0300

    ht8

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-// import { Switch, Route } from 'react-router-dom';
-import Phonebook from 'views/PhonebookView';
+import { Switch, Route } from 'react-router-dom';
 import AppBar from 'components/AppBar';
+import HomeViev from 'views/HomeView';
+import RegisterView from 'views/RegisterView';
+import LoginView from 'views/LoginView';
+import Phonebook from 'views/PhonebookView';
 
 const App = () => {
   return (
     <div>
       <AppBar />
-      <Phonebook />
+
+      <Switch>
+        <Route exact path="/" component={HomeViev} />
+        <Route path="/register" component={RegisterView} />
+        <Route path="/login" component={LoginView} />
+        <Route path="/phonebook" component={Phonebook} />
+      </Switch>
     </div>
   );
 };
--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -2,12 +2,21 @@ import Navigation from './Navigation';
 import UserMenu from './UserMenu';
 import AuthNav from './AuthNav';
 
+const styles = {
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottom: '1px solid #2A363B',
+  },
+};
+
 export default function AppBar() {
   return (
-    <header>
+    <header style={styles.header}>
       <Navigation />
-      <UserMenu />
       <AuthNav />
+      <UserMenu />
     </header>
   );
 }
